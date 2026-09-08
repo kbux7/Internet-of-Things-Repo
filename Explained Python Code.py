1767583b29d797585d46990653471cd4a8cca40b
Karim Bux <kbux7790@gmail.com>
2026-09-08 20:22:56 +0500

    Add files via upload

---
+++ Explained Python Code.py
@@ -0,0 +1,12 @@
+# 5. What does the following Python code do?
+# Explain the type conversion that happens in this program.
+
+age = 20    # Here it stores integer data
+item = 5    # Here it stores integer data
+print("age : ",age)    # Here it prints it as integer data
+print("items : ", item)    # Here it prints as integer data
+
+age = float(age)    # Here it converts integer data to decimal data
+item = float(item)   # Here it converts integer data to decimal data
+print("age : ",age)   # Here it prints data as decimal data
+print("items: ", item)   # Here it prints data as decimal data
