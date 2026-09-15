ea545734a2bd48544cb5dc04172fed43d11df3d7
Karim Bux <kbux7790@gmail.com>
2026-09-15 15:44:04 +0500

    Refactor total pay calculation program

--- Lab2.Total Pay.py
+++ Lab2.Total Pay.py
@@ -1,8 +1,8 @@
-#1. Write a Python program that asks the user to enter the number of hours worked and the hourly rate.
-#Calculate the total pay according to the following rules:
-#If the employee works 40 hours or less, calculate pay as hours × rate.
-#If the employee works more than 40 hours, calculate the first 40 hours at the normal rate and the overtime hours at 1.5 times the hourly rate.
-#Display the total pay.
+# 1. Write a Python program that asks the user to enter the number of hours worked and the hourly rate.
+# Calculate the total pay according to the following rules:
+# If the employee works 40 hours or less, calculate pay as hours × rate.
+# If the employee works more than 40 hours, calculate the first 40 hours at the normal rate and the overtime hours at 1.5 times the hourly rate.
+# Display the total pay.
 
 hours_worked: int = int(input("Enter hours worked:  "))
 hourly_rate = int(input("Enter hourly rate: "))
